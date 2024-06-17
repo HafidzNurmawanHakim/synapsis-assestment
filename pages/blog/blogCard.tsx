7a0fbb27859d6c8f855321b9ac601db27a736429
hafidznurmawanhakim <hafdznurmawan@gmail.com>
2024-06-18 04:15:40 +0700

    fix bugs

--- pages/blog/blogCard.tsx
+++ pages/blog/blogCard.tsx
@@ -12,14 +12,14 @@ export default function BlogCard({ content }: BlogCardProps) {
    return (
       <Card key={content.id} className="my-1 rounded-md">
          <CardHeader>
-            <Link href={`/blog/${content.id}`}>{content.title}</Link>
+            <Link href={`/blog/${content?.id}`}>{content?.title}</Link>
          </CardHeader>
          <CardBody className={clsx(baseFont.className, "text-sm text-gray-400")}>
-            {content.body}
+            {content?.body}
          </CardBody>
          <CardFooter className="gap-2 text-sm text-gray-500">
             <Avatar size="sm" />
-            {content.author ? content.author.name : "Unknown"}
+            {content.author?.name ? content.author.name : "Unknown"}
          </CardFooter>
       </Card>
    );
