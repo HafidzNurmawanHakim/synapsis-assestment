6cc3cc863affcb199e2aa4360a1ef73e2a52202a
hafidznurmawanhakim <hafdznurmawan@gmail.com>
2024-06-18 12:22:28 +0700

    finishing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,7 +18,6 @@ import clsx from "clsx";
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { GithubIcon, SearchIcon, LoginIcon } from "@/components/icons";
-import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
    const searchInput = (
@@ -47,8 +46,7 @@ export const Navbar = () => {
          <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
             <NavbarBrand className="gap-3 max-w-fit">
                <NextLink className="flex justify-start items-center gap-1" href="/">
-                  <Logo />
-                  <p className="font-bold text-inherit">ACME</p>
+                  <p className="font-bold text-inherit">LOGO</p>
                </NextLink>
             </NavbarBrand>
             <div className="hidden lg:flex gap-4 justify-start ml-2">
@@ -75,10 +73,7 @@ export const Navbar = () => {
             </NavbarItem>
             <NavbarItem className="hidden md:flex">
                <Button
-                  isExternal
-                  as={Link}
                   className="text-sm font-normal text-default-600 bg-default-100"
-                  href={siteConfig.links.sponsor}
                   startContent={<LoginIcon width={24} height={24} className="text-danger" />}
                   variant="flat"
                >
--- config/site.ts
+++ config/site.ts
@@ -38,6 +38,5 @@ export const siteConfig = {
       twitter: "https://twitter.com/getnextui",
       docs: "https://nextui-docs-v2.vercel.app",
       discord: "https://discord.gg/9b6yyZKmH4",
-      sponsor: "https://patreon.com/jrgarciadev",
    },
 };
