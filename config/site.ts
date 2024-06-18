693f0475bd8f4c895955fae77ef6c193049f2b4f
hafidznurmawanhakim <hafdznurmawan@gmail.com>
2024-06-18 12:24:56 +0700

    finishing

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-   name: "Next.js + NextUI",
+   name: "Hafidz's Blog",
    description: "Make beautiful websites regardless of your design experience.",
    navItems: [
       {
