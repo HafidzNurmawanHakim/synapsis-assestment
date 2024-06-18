e90d4886c94fd8b76bd33140fdba0fa8d593ce24
Hafidz NH <Hafidznurmawan@gmail.com>
2024-06-18 21:08:42 +0700

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -102,7 +102,7 @@ export const Navbar = () => {
                                 ? "danger"
                                 : "foreground"
                         }
-                        href="#"
+                        href={item.href}
                         size="lg"
                      >
                         {item.label}
